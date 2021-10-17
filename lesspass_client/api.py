93bba4cf074e25d4e68df29a34df6e156055c52b
Kyle Anthony Williams <kyle.anthony.williams2@gmail.com>
2021-10-16 21:05:28 -0400

    Added return types to a couple of methods.

--- lesspass_client/api.py
+++ lesspass_client/api.py
@@ -1,3 +1,4 @@
+from typing import List
 from requests import Session
 
 DEFAULT_API_DOMAIN = 'api.lesspass.com'
@@ -9,20 +10,20 @@ class LessPassClient:
 	but that's for another time.
 	"""
 
-	def __init__(self, email: str, password: str, api_domain: str = DEFAULT_API_DOMAIN, session: Session = Session()):
+	def __init__(self, email: str, password: str, api_domain: str = DEFAULT_API_DOMAIN, session: Session = Session()) -> None:
 		self.email = email
 		self.password = password
 		self.session = session
 		self.api_domain = api_domain
 		self.authenticate(self.email, self.password)
 
-	def authenticate(self, email: str, password: str):
+	def authenticate(self, email: str, password: str) -> None:
 		res = self.session.post(f'https://{self.api_domain}/auth/jwt/create/', json={'email': email, 'password': password})
 		res.raise_for_status()
 
 		self.token = res.json()["access"]
 
-	def passwords(self):
+	def passwords(self) -> List[dict]:
 		res = self.session.get(f'https://{self.api_domain}/passwords/', headers={'Authorization': f'JWT {self.token}'})
 		res.raise_for_status()
 		return res.json()["results"]
