3bfd4327445ef72e481eef25eccf2711dbcc4584
Kyle Anthony Williams <kyle.anthony.williams2@gmail.com>
2021-10-16 20:36:07 -0400

    Edited LessPassClient constructor to allow for customization of API domain.

--- lesspass_client/api.py
+++ lesspass_client/api.py
@@ -1,6 +1,6 @@
 from requests import Session
 
-API_BASE = 'https://api.lesspass.com'
+DEFAULT_API_DOMAIN = 'api.lesspass.com'
 class LessPassClient:
 	"""
 	A very simple implementation of the LessPasss API.
@@ -9,20 +9,21 @@ class LessPassClient:
 	but that's for another time.
 	"""
 
-	def __init__(self, email: str, password: str, session: Session = Session()):
+	def __init__(self, email: str, password: str, api_domain: str = DEFAULT_API_DOMAIN, session: Session = Session()):
 		self.email = email
 		self.password = password
 		self.session = session
+		self.api_domain = api_domain
 		self.authenticate(self.email, self.password)
 
 	def authenticate(self, email: str, password: str):
-		res = self.session.post(f'{API_BASE}/auth/jwt/create/', json={'email': email, 'password': password})
+		res = self.session.post(f'https://{self.api_domain}/auth/jwt/create/', json={'email': email, 'password': password})
 		res.raise_for_status()
 
 		self.token = res.json()["access"]
 
 	def passwords(self):
-		res = self.session.get(f'{API_BASE}/passwords/', headers={'Authorization': f'JWT {self.token}'})
+		res = self.session.get(f'https://{self.api_domain}/passwords/', headers={'Authorization': f'JWT {self.token}'})
 		res.raise_for_status()
 		return res.json()["results"]
 
